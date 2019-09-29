cbf1f0c40562c177dfc0575786113cdf70b7fc20
Rangana Maddumahewa <rbasehewa@gmail.com>
2019-09-29 23:51:00 +0930

    added second exercise

---
+++ secondChallenge/lib/main.js
@@ -0,0 +1,15 @@
+// if(jQuery){
+
+//     alert("Load the jQuery");
+// }else{
+//     alert("NOT LOADED YET");
+// }
+
+$("div").css("background","purple");
+$(".highlight").css("width","200px");
+
+$("#third").css("border", "2px solid orange");
+
+// select the first div only and change its font color to pink
+
+$("div:first-of-type").css("color","pink");
